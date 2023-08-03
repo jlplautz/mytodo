b143016c8b9aecd2eb8cf226bb743e7fe418003d
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-08-03 16:35:09 -0300

    fix(issue-26): Inserted user_name into User model. (#27)

--- todo/db.py
+++ todo/db.py
@@ -5,8 +5,7 @@ from sqlmodel import create_engine
 from todo.config import settings
 
 engine = create_engine(
-    settings.db.uri, echo=settings.db.echo, connect_args=settings.db.connect_args
+    settings.db.uri,
+    echo=settings.db.echo,
+    connect_args=settings.db.connect_args,
 )
-
-# echo=settings.db.echo
-# , connect_ag=settings.db.connect_args

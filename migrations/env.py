bb1c02a9e5ee5451bdc11d36f9ef7898b3842d41
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-08-03 14:59:09 -0300

    feat(issue-23): Inserted into pyproject.toml packages and todo.poetry.scripts. (#23)

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 from todo import models
 from todo.config import settings
